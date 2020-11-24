150e70625f46289f7e7ddab20201043557563c50
aishacodes <aishatabdulfatah81@gmail.com>
2020-11-25 00:01:35 +0100

    ex-1.2

--- part1/courseinfo/src/index.jsx
+++ part1/courseinfo/src/index.jsx
@@ -9,18 +9,23 @@ const Header = ({ course }) => {
 const Total = ({ exercises1, exercises2, exercises3 }) => {
   return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 }
+const Part = ({part, exercise}) => {
+  return(
+    <>
+     <p>
+        {part} {exercise}
+      </p>
+    </>
+  )
+
+}
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+
     </>
   )
 }
