a4a296642b809d8ea5cafae4abb037a5e267014d
aishacodes <aishatabdulfatah81@gmail.com>
2020-12-02 15:03:33 +0100

    ex-1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
+const Botton = ({handleEvent, text}) => {
+  
+  return(
+  <button onClick={handleEvent}>{text}</button>
+  )
+}
+
+
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
   return (
     <div>
-      
+      <h1>give feedback</h1>
+      <Botton handleEvent = {() => setGood(good+1)} text="Good"/>
+      <Botton handleEvent = {() => setNeutral(neutral+1)} text="Neutral"/>
+      <Botton handleEvent = {() => setBad(bad+1)} text="Bad"/>
+      <h1>Statistics</h1>
+        <h3>Good {good}</h3> 
+        <h3>Neutral {neutral}</h3>
+        <h3>Bad {bad}</h3>
     </div>
   )
 }
