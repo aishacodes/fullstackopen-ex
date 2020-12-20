6c9229e3998476412728901cac742a9ead13e592
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2020-12-20 15:34:02 +0100

    ex-2.9 - step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "040 - 1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const AddName = (event) => {
     event.preventDefault();
@@ -22,15 +26,38 @@ const App = () => {
     }
     setPersons([
       ...persons,
-      { name: newName.trim(), phoneNumber: newNumber.trim() },
+      { name: newName.trim(), number: newNumber.trim() },
     ]);
     setNewName("");
     setNewNumber("");
   };
 
+  // const handleFilter = (ev) => {
+  //   console.log(ev);
+  //   ev.preventDefault();
+  //   const query = ev.target.value;
+  //   if (!query.trim()) setFilter(() => null);
+  //   setFilter(
+  //     persons.filter((person) => {
+  //       return (
+  //         person.name.toLowerCase().trim().indexOf(query.trim().toLowerCase()) >
+  //         -1
+  //       );
+  //     })
+  //   );
+  // };
+  const handleFilter = persons.filter(
+    (person) =>
+      person.name.trim().toLowerCase().indexOf(filter.trim().toLowerCase()) > -1
+  );
+
+  const contactToShow = handleFilter.length ? handleFilter : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter shown with:{" "}
+      <input value={filter} onChange={(e) => setFilter(e.target.value)} />
       <form onSubmit={AddName}>
         <div>
           name:{" "}
@@ -49,9 +76,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {contactToShow.map((person) => (
         <p key={person.name}>
-          {person.name} {person.phoneNumber}
+          {person.name} {person.number}
         </p>
       ))}{" "}
     </div>
